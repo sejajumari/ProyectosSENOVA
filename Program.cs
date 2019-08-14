1649090b9ad73a26ab09b299b237f1406f5147a1
unknown <Sala300@TUNDFPCGE10F373.FormacionSena.RED>
2019-08-14 16:56:12 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProyectosSENOVA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int edad;
+            //int puntaje=0;
+            //Proyecto miProyecto = new Proyecto();
+            //miProyecto.Area = "sistemas";
+            //miProyecto.Codigo = 12000;
+            //miProyecto.Duracion = 13;
+            //miProyecto.Nombre = "Desarrollo internet de las cosas IOT";
+
+            //Console.WriteLine($"Area:{miProyecto.Area}");
+            //Console.WriteLine($"Codigo:{miProyecto.Codigo}");
+            //Console.WriteLine($"Duracion:{miProyecto.Duracion}");
+            //Console.WriteLine($"Nombre:{miProyecto.Nombre}");
+
+            //Proyecto miProyecto1 = new Proyecto();
+            //miProyecto1.Area = "Desarollo";
+            //miProyecto1.Codigo = 14000;
+            //miProyecto1.Duracion = 16;
+            //miProyecto1.Nombre = "Desarrollo Informacion";
+
+            //Console.WriteLine($"Area:{miProyecto1.Area}");
+            //Console.WriteLine($"Codigo:{miProyecto1.Codigo}");
+            //Console.WriteLine($"Duracion:{miProyecto1.Duracion}");
+            //Console.WriteLine($"Nombre:{miProyecto1.Nombre}");
+
+
+            //Proyecto miProyecto2 = new Proyecto();
+            Proyecto miProyecto3 = new Proyecto();
+            miProyecto3.IngresarPrpyecto();
+
+
+            Console.WriteLine("Ingrese la cantidad de cifras para el codigo");
+            int x = int.Parse(Console.ReadLine());
+
+            miProyecto3.ObtetenerMayores(x);
+
+            List<Proyecto> proyectos = new List<Proyecto>();
+            miProyecto3.ImprimirProyecto(proyectos);
+
+            //Proyecto miProyecto4 = new Proyecto("Realidad Aumentada", 2019, "Tecnologia", 1110);
+            //miProyecto4.ImprimirProyecto();
+
+
+            //Proyecto miProyecto5 = new Proyecto("Talento Humano", 2020, "SOCIAL", 1220);
+            //miProyecto5.ImprimirProyecto();
+
+           
+            Console.ReadLine();
+
+
+            //Entendido...
+
+        }
+    }
+}
---
+++ Proyecto.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectosSENOVA
+{
+    public class Proyecto
+    {
+        #region Atributos
+        public List<Proyecto> proyectos = new List<Proyecto>();
+        #endregion
+
+
+        #region Propieades
+        public string Nombre { get; set; }
+        public double Codigo { get; set; }
+        public string Area   { get; set; }
+        public int Duracion  { get; set; }
+        #endregion
+        #region Contructores
+        public Proyecto(string nombre, double codigo, string area, int duracion)
+        {
+            Nombre = nombre;
+            Codigo = codigo;
+            Area = area;
+            Duracion = duracion;
+
+        }
+        public Proyecto(string area, int duracion)
+        {
+            Area = area;
+            Duracion = duracion;
+        }
+        public Proyecto()
+        {
+
+        }
+        #endregion
+        #region Metodos
+        public void ObtetenerMayores(int x)
+        {
+           List<Proyecto> pro20 = new List<Proyecto>();
+            
+            foreach (var item in proyectos)
+            {
+                if (item.Codigo>x)
+                {
+                    pro20.Add(item);  
+                }
+            }
+            ImprimirProyecto(pro20);
+        }
+
+        public void IngresarPrpyecto()
+        {
+            var res = "y";
+            while (res=="y")
+            {
+                Proyecto miproyecto = new Proyecto();
+                Console.WriteLine("Ingrese el Nombre del Proyecto");
+                miproyecto.Nombre = Console.ReadLine();
+                Console.WriteLine("Ingrese el Codigo");
+                miproyecto.Codigo = double.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese el Area");
+                miproyecto.Area = Console.ReadLine();
+                Console.WriteLine("Ingrese el Duracion");
+                miproyecto.Duracion = int.Parse(Console.ReadLine());
+                proyectos.Add(miproyecto);
+                Console.WriteLine("Desea agregar otro proyecto");
+                res = Console.ReadLine();
+            }
+        }
+
+        public void ImprimirProyecto(List<Proyecto> proyecto1)
+        {
+            foreach (var item in proyecto1)
+            {
+            Console.WriteLine($"El Nombre del proyecto es:{item.Nombre}");
+            Console.WriteLine($"El Codigo del proyecto es:{item.Codigo}");
+            Console.WriteLine($"El Area del proyecto es:{item.Area}");
+            Console.WriteLine($"La durecion del proyecto es:{item.Duracion}");
+            }
+        }
+        #endregion
+
+
+    }
+}
+//obtener todos los proyectos cuyo codigo sea mayor a 20 y sea numero par.
+//ordenar los proyectos ascendentemente por duracion.
+//seleccionar el primer proyecto que sea multiplo de 6.
+//listar solo la areas de los proyectos.
+//seleccionar los proyectos solo con nombre y codigo.
+//sumar la duracion de todos los proyectos.
+//agrupar los proyectos por proyectos.
+
